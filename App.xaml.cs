24681dbf98d8864a57d37b30e5020211a2b7ff20
Ole2710 <56429293+Ole2710@users.noreply.github.com>
2021-12-18 22:32:31 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MusicLibrary
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Link naar database
+        static string strDatabaseName = "Music Library.db";
+        static string strFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string strDatabasePath = System.IO.Path.Combine(strFolderPath, strDatabaseName);
+    }
+}
---
+++ LoginPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SQLite;
+
+namespace MusicLibrary
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationPage registrationPage = new RegistrationPage();
+            this.Hide();
+            registrationPage.ShowDialog();
+        }
+
+        private void btnLoginClick(object sender, RoutedEventArgs e)
+        {
+            /*SqlConnection sqlCon = new SqlConnection(@"Data Source=C:\Users\oleme\source\repos\MusicLibrary; Initial Catalog = Music_Library; Integrated Security = True;");
+            try
+            {
+                if(sqlCon.State)
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+            
+            }*/
+
+            SQLiteConnection connection = new SQLiteConnection(App.strDatabasePath);
+            {
+                //connection.CreateTable<Login>();
+                //connection.Update(Login);
+            }
+            Close();
+
+        }
+    }
+}
